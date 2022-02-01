373f2d3c318263286973b3f1e707ed858d0d2a38
Dimitar Radulov <dimitaradulov@gmail.com>
2022-02-01 14:34:46 +0200

    Add hamburger menu on small screens

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const hamburger = document.querySelector('.nav__hamburger');
+const navLinks = document.querySelector('.nav__links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
