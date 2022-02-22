b048b860abcd06273e611d0b00a9b90c5576bd1e
Dimitar Radulov <dimitaradulov@gmail.com>
2022-02-22 13:44:54 +0200

    Add sticky nav rootMargin

--- js/script.js
+++ js/script.js
@@ -115,16 +115,20 @@ const imageObserver = new IntersectionObserver(
 images.forEach((img) => imageObserver.observe(img));
 
 // Sticky Navigation
-const headerObserverCb = (entries) => {
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
   const [entry] = entries;
+  // console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
 
-const headerObserver = new IntersectionObserver(headerObserverCb, {
+const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
 
 headerObserver.observe(header);
